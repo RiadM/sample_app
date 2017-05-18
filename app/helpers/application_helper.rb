ea16e5f2706cd3397ba9ff3976b597f7a13d81a1
Riad Mahdid <r.mahdid13+git@gmail.com>
2017-05-18 13:23:30 -0400

    Finit le chapitre et ajout du full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#Retourne un titre complet selon les pages
+	def full_title(page_title = '')				#Method def, optional arg
+		base_title = "Ruby on Rails"			# Variable assignement
+		if page_title.empty?					# Boolean test
+			base_title							# Implicit return
+		else
+			page_title + " | " + base_title		# String Concatenation
+		end
+	end
 end
