da8c9358e7f98f993d27ace61a2c7744905cf7a8
Riad Mahdid <r.mahdid13+git@gmail.com>
2017-07-07 17:27:24 -0400

    Rajout du feed et du following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -84,4 +84,28 @@ class UserTest < ActiveSupport::TestCase
 			@user.destroy
 		end
 	end
+
+
+	#TEST 14.3
+
+	test "feed should have the right posts" do
+		michael 	= users(:michael)
+		archer		= users(:archer)
+		lana		= users(:lana)
+		#posts from the followed user
+		lana.microposts.each do |post_following|
+			assert michael.feed.include?(post_following)
+		end
+
+		#posts from self
+		michael.microposts.each do |post_self|
+			assert michael.feed.include?(post_self)
+		end
+
+		#posts from unfollowed user
+		archer.microposts.each do |post_unfollowed|
+			assert_not michael.feed.include?(post_unfollowed)
+		end
+	end
+
 end
